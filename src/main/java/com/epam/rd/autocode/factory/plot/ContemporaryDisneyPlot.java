70e5b82400b3fc2db4a0451af03a902234bc8355
Lyrond <abylayhan0104@gmail.com>
2024-01-28 21:20:03 +0600

    commit

--- src/main/java/com/epam/rd/autocode/factory/plot/ContemporaryDisneyPlot.java
+++ src/main/java/com/epam/rd/autocode/factory/plot/ContemporaryDisneyPlot.java
@@ -14,6 +14,6 @@ public class ContemporaryDisneyPlot implements Plot {
     @Override
     public String toString() {
         return hero.name() + " feels a bit awkward and uncomfortable. But personal issues fades, when a big trouble comes - " + epicCrisis.name() + ". " + hero.name() + " stands up against it, but with no success at first.But putting self together and help of friends, including spectacular funny " +
-                funnyFriend.name() + " restore the spirit and " + hero.name() + " overcomes the crisis and gains gratitude and respect";
+                funnyFriend.name() + "restore the spirit and " + hero.name() + " overcomes the crisis and gains gratitude and respect";
     }
 }
